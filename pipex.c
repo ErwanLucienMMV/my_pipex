bb4ac9412cdec977889a701643ca7f72d4dde9f8
Erwan Maigne <emaigne@f1br5s3.clusters.42paris.fr>
2026-01-06 12:30:01 +0100

    git branlette

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 13:41:11 by emaigne           #+#    #+#             */
-/*   Updated: 2026/01/06 11:08:22 by emaigne          ###   ########.fr       */
+/*   Updated: 2026/01/06 11:49:16 by emaigne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ int	main(int argc, char **argv, char **env)
 {
 	if (argc != 5)
 		return (1);
+	if (access(argv[1], R_OK) == -1)
+		return(ft_printf(""))
 	return (0);
 }
